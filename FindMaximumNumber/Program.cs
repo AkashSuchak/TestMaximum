9e788ded6ffeca3cb686c9a8478a9eb5a9a4d1bf
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-08-12 17:44:51 +0530

    feat: Added : Find Maximum from 3 Ineger Numbers

--- FindMaximumNumber/Program.cs
+++ FindMaximumNumber/Program.cs
@@ -8,6 +8,13 @@ namespace FindMaximumNumber
         {
             //Welcome Message
             Console.WriteLine("Welcome to Maximum Number");
+
+            //Calling Class to call a Function
+            MaximumNumberCheck maximumNumberCheck = new MaximumNumberCheck();
+            int outputInteger = maximumNumberCheck.MaximumIngerNumber(11, 22, 33);
+            
+            //Display Output
+            Console.WriteLine("Maximum Number of Integer : " + outputInteger);
         }
     }
 }
