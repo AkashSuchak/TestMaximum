87503f6996255e5d57eddbea2ca3f5d50efd658a
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-08-12 17:24:35 +0530

    feat : Welcome Message

---
+++ FindMaximumNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FindMaximumNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome Message
+            Console.WriteLine("Welcome to Maximum Number");
+        }
+    }
+}
