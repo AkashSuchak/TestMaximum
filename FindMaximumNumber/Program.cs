5d455e2cc910e99efb25106c5acc07d1fe9479cd
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-08-12 18:03:43 +0530

    feat: Added : Find Maximum from 3 Float Numbers with TestCases

--- FindMaximumNumber/Program.cs
+++ FindMaximumNumber/Program.cs
@@ -11,12 +11,21 @@ namespace FindMaximumNumber
 
             //Calling Class to call a Function
             MaximumNumberCheck maximumNumberCheck = new MaximumNumberCheck();
-            
+
+            //Maximum Number At First Place
+            double outputFloat1 = maximumNumberCheck.MaximumFloatNumber(14.1, 2.82, 7.33);
+            Console.WriteLine("Maximum Number of Float at First Place : " + outputFloat1);
+
+            //Maximum Number At Second Place
+            double outputFloat2 = maximumNumberCheck.MaximumFloatNumber(4.1, 28.2, 12.8);
+            Console.WriteLine("Maximum Number of Integer at Second Place : " + outputFloat2);
+
             //Maximum Number At Third Place
-            int outputInteger = maximumNumberCheck.MaximumIngerNumber(141, 282, 733);
-            
-            //Display Output
-            Console.WriteLine("Maximum Number of Integer : " + outputInteger);
+            double outputFloat3 = maximumNumberCheck.MaximumFloatNumber(75.9, 68.47, 77.3);
+            Console.WriteLine("Maximum Number of Integer at Third Place : " + outputFloat3);
+
+
+
 
 
         }
