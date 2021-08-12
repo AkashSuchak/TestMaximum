176cad116573e9fee2c69bb505fbdc7b4a7fa11d
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-08-12 17:50:23 +0530

    feat : TC-1.1-Max Number at First Place

--- FindMaximumNumber/Program.cs
+++ FindMaximumNumber/Program.cs
@@ -11,10 +11,14 @@ namespace FindMaximumNumber
 
             //Calling Class to call a Function
             MaximumNumberCheck maximumNumberCheck = new MaximumNumberCheck();
-            int outputInteger = maximumNumberCheck.MaximumIngerNumber(11, 22, 33);
+            
+            //Maximum Number At First Place
+            int outputInteger = maximumNumberCheck.MaximumIngerNumber(411, 282, 333);
             
             //Display Output
             Console.WriteLine("Maximum Number of Integer : " + outputInteger);
+
+
         }
     }
 }
