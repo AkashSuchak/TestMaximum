6a62d8c85f0f7e01b8f4b21e0768895789b91c37
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-08-12 18:58:29 +0530

    feat: refactor : Using Generic Method Find Maximum Number

--- FindMaximumNumber/MaximumNumberCheck.cs
+++ FindMaximumNumber/MaximumNumberCheck.cs
@@ -6,7 +6,7 @@ namespace FindMaximumNumber
 {
     class MaximumNumberCheck
     {
-        //Function To check Maximum Number
+        /*//Function To check Maximum Number
         public int MaximumIngerNumber(int firstValue, int secondValue, int thirdValue)
         {
             //Comaring 3 Numbers
@@ -59,7 +59,23 @@ namespace FindMaximumNumber
                 return thirdValue;
             }
             throw new Exception("First, Second and Third number are same : ");
+        }*/
+        //Function To check Maximum Number Using Generics Method
+        public T MaximumNumber<T>(T firstValue, T secondValue, T thirdValue) where T : IComparable
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("First, Second and Third number are same : ");
         }
-
     }
 }
